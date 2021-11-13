709ffb294ba563468fb06930d1acbdf1636c0f39
thor56 <ranjithkmr598@gmail.com>
2021-11-13 23:05:29 +0530

    Basic Test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/test", methods=['POST'])
+def test():
+    return json.loads(request.data)
