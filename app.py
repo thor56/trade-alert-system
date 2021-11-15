f02b80bc352f6eb29eae778280f19aca60a935d8
thor56 <ranjithkmr598@gmail.com>
2021-11-16 00:27:34 +0530

    Revert "Second Phase"
    This reverts commit b7cbf759b12a982ace5c96920c916aa40936044d.

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-from flask import Flask, json, request, url_for
-from flask_sqlalchemy import SQLAlchemy
-
-import os
+from flask import Flask, json, request
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE'] = os.environ.get("DATABASE_URL")
-
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-
-
-db.init_app()
 
 
 @app.route("/")
@@ -26,8 +10,6 @@ def hello_world():
 
 @app.route("/test", methods=['POST'])
 def test():
-    data = json.loads(request.data)
-    user = User(name=data["exchange"], email=data["ticker"])
-    db.session.add(user)
-    db.session.commit()
-    return data
+    data1 = json.loads(request.data)
+    print(data1)
+    return data1
