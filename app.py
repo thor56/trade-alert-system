b7cbf759b12a982ace5c96920c916aa40936044d
thor56 <ranjithkmr598@gmail.com>
2021-11-16 00:08:34 +0530

    Second Phase

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, json, request
+from flask import Flask, json, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE'] = os.environ.get("DATABASE_URL")
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+
+db.init_app()
 
 
 @app.route("/")
@@ -10,6 +26,8 @@ def hello_world():
 
 @app.route("/test", methods=['POST'])
 def test():
-    data1 = json.loads(request.data)
-    print(data1)
-    return data1
+    data = json.loads(request.data)
+    user = User(name=data["exchange"], email=data["ticker"])
+    db.session.add(user)
+    db.session.commit()
+    return data
