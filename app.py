7ad1b7ff79abede61571f35b2b6ac3f62cf509fd
thor56 <ranjithkmr598@gmail.com>
2021-11-16 17:33:46 +0530

    DB Operation Working

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask, json, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+import os
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123@localhost/users"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
+db = SQLAlchemy(app)
+CORS(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    age = db.Column(db.String(255))
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return '%s/%s' % (self.name, self.age)
 
 
 @app.route("/")
@@ -10,6 +33,13 @@ def hello_world():
 
 @app.route("/test", methods=['POST'])
 def test():
-    data1 = json.loads(request.data)
-    print(data1)
-    return data1
+    data = json.loads(request.data)
+    user = User(data['exchange'], data['ticker'])
+    db.session.add(user)
+    db.session.commit()
+
+    return json.jsonify({
+        'status': 'Data is posted to PostgreSQL!',
+        'name': data['exchange'],
+        'age': data['exchange']
+    })
